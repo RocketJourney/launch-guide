23f09fbcd174d2856d348e3600c469930df0f177
Gustavo <gmora008@gmail.com>
2018-05-29 10:31:53 -0500

    make build

--- docs/_next/static/commons/manifest.js
+++ docs/_next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "a01e6dad54bc8c07f44c"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "5e890e9f73fa199b7b42"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"bundles/pages/_app.js","4":"bundles/pages/index.js"}[chunkId]||chunkId) + "-" + {"0":"f5e4cd4d64fc9d47b85f","1":"9b03ff85b27d0d91c43b","2":"3866a81171a60eba66b9","4":"deeac72a4804940d86ec"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"bundles/pages/_app.js","4":"bundles/pages/index.js"}[chunkId]||chunkId) + "-" + {"0":"f5e4cd4d64fc9d47b85f","1":"9b03ff85b27d0d91c43b","2":"3866a81171a60eba66b9","4":"8001e6781fbd95f6dfce"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
